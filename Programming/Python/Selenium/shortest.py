039322a15b1a18614e58d8d0320773281caba38e
alaricespina <alaricsespina@gmail.com>
2020-10-28 16:24:10 +0800

    123

--- Programming/Python/Selenium/shortest.py
+++ Programming/Python/Selenium/shortest.py
@@ -8,72 +8,75 @@ from webdriver_manager.chrome import ChromeDriverManager
 import random
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-waitdelay = 10 #in seconds
+for i in range(0,10):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
 
-def findByLinkText(LT):
-    return driver.find_element_by_link_text(LT)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    waitdelay = 10 #in seconds
 
-def findByID(Id):
-    return driver.find_element_by_id(Id)
+    def findByLinkText(LT):
+        return driver.find_element_by_link_text(LT)
 
-def findByClass(Classname):
-    return driver.find_element_by_class_name(Classname)
+    def findByID(Id):
+        return driver.find_element_by_id(Id)
 
-def waitForLinkText(LT):
+    def findByClass(Classname):
+        return driver.find_element_by_class_name(Classname)
+
+    def waitForLinkText(LT):
+        try:
+            WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.LINK_TEXT,LT)))
+        except:
+            print("Error")
+
+    def waitForID(Id):
+        try:
+        WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.ID,Id)))
+        except:
+            print("Error")
+
+    def waitForClass(Classname):
+        try:
+        WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.CLASS_NAME,Classname)))
+        except:
+            print("Error")
+
+
+
+    linktxt = open("Hatdog.txt")
+    links = [i for i in linktxt]
+    for x in links:
+        print(x)
+
+    linktxt.close()
+    print(links)
+
+    CLI = random.randint(0,len(links)-1)
+    target = links[CLI]
+    print(target)
+
+    driver.get(target)
+    print("Sleeping for 10")
+    time.sleep(10)
+    print("Sleep done")
     try:
-        WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.LINK_TEXT,LT)))
+        WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.ID,"rc-imageselect")))
+        driver.close()
     except:
-        print("Error")
+        print("No Captcha")
 
-def waitForID(Id):
+    waitForID("skip_button")
+    enter = findByID("skip_button")
     try:
-       WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.ID,Id)))
+        enter.click()
     except:
-        print("Error")
+        print("Thingy not clickable")
 
-def waitForClass(Classname):
-    try:
-       WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.CLASS_NAME,Classname)))
-    except:
-        print("Error")
+    print("Enter Clicked")
 
-
-
-linktxt = open("Hatdog.txt")
-links = [i for i in linktxt]
-for x in links:
-    print(x)
-
-linktxt.close()
-print(links)
-
-CLI = random.randint(0,len(links)-1)
-target = links[CLI]
-print(target)
-
-driver.get(target)
-print("Sleeping for 10")
-time.sleep(10)
-print("Sleep done")
-try:
-    WebDriverWait(driver, waitdelay).until(EC.visibility_of_element_located((By.ID,"rc-imageselect")))
     driver.close()
-except:
-    print("No Captcha")
+    driver.quit()
 
-waitForID("skip_button")
-enter = findByID("skip_button")
-try:
-    enter.click()
-except:
-    print("Thingy not clickable")
-
-print("Enter Clicked")
-
-driver.close()
-driver.quit()
+    print("Driver Has Exited")
